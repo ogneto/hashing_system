d2a91a364feab2e73ace497365dd4f3ff1e5ea6c
OgNeto <ogsilvanetu@gmail.com>
2025-07-01 19:39:50 -0300

    Student Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeacherModule } from './teacher/teacher.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [TeacherModule],
+  imports: [TeacherModule, StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
