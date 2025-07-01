075f9480561f559fe8f6f91eb50600a3f9f501ab
OgNeto <ogsilvanetu@gmail.com>
2025-07-01 20:02:00 -0300

    Environment Variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,30 @@ import { AppService } from './app.service';
 import { TeacherModule } from './teacher/teacher.module';
 import { StudentModule } from './student/student.module';
 import { CourseModule } from './course/course.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TeacherModule, StudentModule, CourseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: config.get('DATABASE_TYPE') as 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: Number(config.get<string>('DATABASE_PORT')),
+        username: config.get<string>('DATABASE_USERNAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_DATABASE'),
+        autoLoadEntities: config.get<boolean>('DATABASE_AUTOLOADENTITIES'),
+        synchronize: config.get<boolean>('DATABASE_SYNCHRONIZE')
+      })
+    }),
+    TeacherModule,
+    StudentModule,
+    CourseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
